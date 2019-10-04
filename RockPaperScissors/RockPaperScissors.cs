59f871cbea5c7fd62bdba1b67086ceeae025592f
Youssef Shoaib <canonballt@gmail.com>
2019-10-04 15:44:29 -0400

    Finish logic

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -10,18 +10,46 @@ using System.Windows.Forms;
 
 namespace RockPaperScissors
 {
-    public partial class RockPaperScissors : Form
-    {
-        Player playerUser = new Player(Winner.User), playerComputer = new Player(Winner.Computer);
+	public partial class RockPaperScissors : Form
+	{
+		Player playerUser = new Player(Winner.User), playerComputer = new Player(Winner.Computer);
+		Random rGen = new Random();
 
-        public RockPaperScissors()
-        {
-            InitializeComponent();
-        }
+		public RockPaperScissors()
+		{
+			InitializeComponent();
+		}
 
-        private void btnChoice_Click(object sender, EventArgs e)
-        {
-            playerUser.Choice = (Choice)Convert.ToInt32(((Button)sender).Tag);
-        }
-    }
+		private void btnChoice_Click(object sender, EventArgs e)
+		{
+			Winner winner = Winner.None;
+			playerUser.Choice = (Choice)Convert.ToInt32(((Button)sender).Tag);
+			playerComputer.Choice = (Choice)rGen.Next(1, 4);
+
+			if (playerUser.Choice == playerComputer.Choice)
+				winner = Winner.Draw;
+			else if (playerUser.Choice.CanBeat(playerComputer.Choice))
+				winner = Winner.User;
+			else
+				winner = Winner.Computer;
+
+			playerUser.OnWinnerAnnounced(winner);
+			playerComputer.OnWinnerAnnounced(winner);
+			DisplayStats();
+			DisplayWinner(winner);
+		}
+
+		private void DisplayWinner(Winner winner)
+		{
+			MessageBox.Show(winner.GetDescription(), "Winner");
+			playerUser.Choice = Choice.None;
+			playerComputer.Choice = Choice.None;
+		}
+
+		private void DisplayStats()
+		{
+			lblUserStats.Text = "User Stats: \n" + playerUser.ToString();
+			lblComputerStats.Text = "Computer Stats: \n" + playerComputer.ToString();
+		}
+	}
 }
